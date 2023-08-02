ad0f35a3c502d1106c0c6dc7c570d4b19f7126c0
Robert-Iskandarani <riskandarani@fi.uba.ar>
2023-08-02 11:43:25 -0300

    pagina terminada

--- src/sections/Data.tsx
+++ src/sections/Data.tsx
@@ -6,6 +6,19 @@ import Separator from '../components/Separator';
 
 import { MdOutlineKeyboardArrowDown } from 'react-icons/md';
 
+import Bren from './../assets/Bren.jpeg';
+import Caro from './../assets/Caro.jpeg';
+import Eve from './../assets/Eve.jpeg';
+import FedeKaplun from './../assets/FedeKaplun.jpeg';
+import Florte from './../assets/Florte.jpeg';
+import JuliGelis from './../assets/JuliGelis.jpeg';
+import Juze from './../assets/Juze.jpeg';
+import Lara from './../assets/Lara.jpeg';
+import Martu from './../assets/Martu.jpeg';
+import Mica from './../assets/Mica.jpeg';
+import Robert from './../assets/Robert.jpeg';
+import Renu from './../assets/Renu.jpeg';
+
 interface DataProps {
   name: string;
   setName: Dispatch<SetStateAction<string>>;
@@ -47,7 +60,7 @@ const Data = ({ name, setName, artist, setArtist }: DataProps) => {
             <div className={styles.options}>
               <div
                 className={styles.option}
-                onClick={() => setArtist('Trinche')}
+                onClick={() => setArtist('El Trinche')}
               >
                 El Trinche
               </div>
@@ -72,6 +85,20 @@ const Data = ({ name, setName, artist, setArtist }: DataProps) => {
             </div>
           )}
         </div>
+      </div>
+      <div className={styles.images}>
+        <img className={styles.image} src={Bren} alt='image' />
+        <img className={styles.image} src={Caro} alt='image' />
+        <img className={styles.image} src={Eve} alt='image' />
+        <img className={styles.image} src={FedeKaplun} alt='image' />
+        <img className={styles.image} src={Florte} alt='image' />
+        <img className={styles.image} src={JuliGelis} alt='image' />
+        <img className={styles.image} src={Juze} alt='image' />
+        <img className={styles.image} src={Lara} alt='image' />
+        <img className={styles.image} src={Martu} alt='image' />
+        <img className={styles.image} src={Mica} alt='image' />
+        <img className={styles.image} src={Robert} alt='image' />
+        <img className={styles.image} src={Renu} alt='image' />
       </div>
     </div>
   );
